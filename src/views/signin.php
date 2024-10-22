13c866c2b4592f65bd6656108e374438deea0c98
dustedge <schnapps_work@proton.me>
2024-10-23 01:36:03 +0300

    Added temporary design to signin page.

---
+++ src/views/signin.php
@@ -0,0 +1,37 @@
+<DOCTYPE !html>
+<html>
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Lora">
+<link rel="stylesheet" href="css/styles1.css" />
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+<div class="flex-container-centered ds-dashed-border" style="flex-direction:row;">
+
+    <div class="flex-container-centered">
+        <h1 class="ds-logo-container">BREVITY</h1>
+    </div>
+    
+    <div class="flex-container-centered" style="align-items:stretch;">
+        <div>
+            <h1 style="margin:1vh;">Sign In:</h1>
+        </div>
+        <div>
+            <input class="ds-text-field" type="email" style="margin:1vh;" placeholder="Hello@to.you"/>
+        </div>
+        <div>
+            <input class="ds-text-field" type="password" style="margin:1vh;" placeholder="Password"/>
+        </div>
+        <div>
+            <button class="ds-button" style="width:95%;margin:1vh 1vh;"><h3 style="font-size:100%;">Sign In</h3></button>
+        </div>
+        <div>
+            <p style="display:block;margin:1vh;text-align:center;">Don't have an account?</p>
+            <a href="/register" style="display:block;margin:1vh;text-align:center;">Sign Up</a>
+        </div>
+    </div>
+
+</div>
+</body>
+</html>
