a3b56b0c9207c6ec8dec041d0e5043c852af391f
dustedge <schnapps_work@proton.me>
2024-10-24 21:58:50 +0300

    Moved button functionality. Added home button

---
+++ public/js/ds-common-buttons.js
@@ -0,0 +1,59 @@
+// Common button functionality
+
+buttonSignIn = document.getElementById("buttonSignIn");
+
+if(buttonSignIn != null) {
+    buttonSignIn.addEventListener('click', function() {
+        window.location.href = "/sign-in";
+    });
+}
+
+buttonLogout = document.getElementById("buttonLogout");
+
+if(buttonLogout != null) {
+    buttonLogout.addEventListener('click', function() {
+        window.location.href = "/logout";
+    });
+}
+
+buttonUserProfile = document.getElementById("buttonUserProfile");
+
+if(buttonUserProfile != null) {
+    buttonUserProfile.addEventListener('click', function() {
+        window.location.href = "/user";
+    });
+}
+
+// Modal post button
+// Aquire button
+const postButton = document.getElementById('postButton');
+if(postButton != null) {
+    const postWindowModal = document.getElementById('postWindowModal');
+    const closeWindowModal = document.querySelector('.close');
+
+    // Open modal window when "Post" is pressed
+    postButton.addEventListener('click', function()
+    {
+        postWindowModal.style.display = 'block';
+    });
+
+    // Close modal window when close element is pressed
+    closeWindowModal.addEventListener('click', function() {
+        postWindowModal.style.display = 'none'; 
+    });
+
+    // Close modal window when clicked outside
+    window.addEventListener('click', function(event) {
+        if(event.target === postWindowModal) {
+            postWindowModal.style.display = 'none';
+        }
+    });
+}
+
+const homeButton = document.getElementById("homeButton");
+if (homeButton != null)
+{
+    homeButton.addEventListener('click', function() {
+        window.location.href="/";
+    });
+}
