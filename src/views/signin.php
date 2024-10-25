7ff4fe4e689783f01eac3997fe35ed0680e03fe3
dustedge <schnapps_work@proton.me>
2024-10-25 20:23:31 +0300

    Added form tags

--- src/views/signin.php
+++ src/views/signin.php
@@ -14,24 +14,25 @@
     </div>
     
     <div class="flex-container-centered" style="align-items:stretch;">
-        <div>
-            <h1 style="margin:1vh;">Sign In:</h1>
-        </div>
-        <div>
-            <input class="ds-text-field" type="email" style="margin:1vh;" placeholder="Hello@to.you"/>
-        </div>
-        <div>
-            <input class="ds-text-field" type="password" style="margin:1vh;" placeholder="Password"/>
-        </div>
-        <div>
-            <button class="ds-button" style="width:95%;margin:1vh 1vh;" id="buttonLogin"><h3 style="font-size:100%;">Sign In</h3></button>
-        </div>
+        <form action="/sign-in-handler" method="POST">
+            <div>
+                <h1 style="margin:1vh;">Sign In:</h1>
+            </div>
+            <div>
+                <input class="ds-text-field" type="email" style="margin:1vh;" placeholder="Hello@to.you" required/>
+            </div>
+            <div>
+                <input class="ds-text-field" type="password" style="margin:1vh;" placeholder="Password" required/>
+            </div>
+            <div>
+                <button class="ds-button" type="submit" value="Submit" style="width:95%;margin:1vh 1vh;" id="buttonLogin"><h3 style="font-size:100%;">Sign In</h3></button>
+            </div>
+        </form>
         <div>
             <p style="display:block;margin:1vh;text-align:center;">Don't have an account?</p>
             <a href="/register" style="display:block;margin:1vh;text-align:center;">Sign Up</a>
         </div>
     </div>
-
 </div>
 <script src="js/ds-common-buttons.js"></script>
 </body>
