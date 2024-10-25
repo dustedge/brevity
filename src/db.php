dcf1eeabb2b62af58fd091d90604b24ef1002a41
dustedge <schnapps_work@proton.me>
2024-10-25 20:20:13 +0300

    Database connection file

---
+++ src/db.php
@@ -0,0 +1,23 @@
+<?php
+
+// Database connection
+$host = 'localhost';
+$db = 'bravity_db';         // Template
+$user = 'admin';            // Template
+$pass = 'bravityadmin321';  // Template
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+// pdo prevents sql injections
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
