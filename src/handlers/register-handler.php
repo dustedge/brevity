e9ece1cc5ea20cff7dc5f0712f66b19d210fdce1
dustedge <schnapps_work@proton.me>
2024-10-26 01:51:05 +0300

    Added registration functionality

--- src/handlers/register-handler.php
+++ src/handlers/register-handler.php
@@ -7,6 +7,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = trim($_POST['password']);
     $passwordConfirm = trim($_POST['passwordConfirm']);
     $login = htmlspecialchars(trim($_POST['login']));
+    $avatar = "/images/avatar-none.png";
 
     if($password != $passwordConfirm) {
         echo "POST: Error. Passwords don't match.";
@@ -23,15 +24,35 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     // connect to db
     require_once __DIR__ . '/../db.php';
 
-    $stmt = $pdo->prepare("INSERT INTO users (username, usertag, email, password) VALUES (:displayName, :login, :email, :password)");
+
+    // check if user with same email exists
+    $check = $pdo->prepare("SELECT * FROM users WHERE email = :email LIMIT 1");
+    $check->bindParam(":email", $email);
+    try {
+        $check->execute();
+        $checkuser = $check->fetch();
+        if($checkuser) {
+            echo "POST: Error. Email taken.";
+            exit;
+        }
+    }
+    catch(PDOException $e) {
+        echo "POST: SQL Query Error: " . $e->getMessage();
+        exit;
+    }
+
+    // prepare register query
+    $stmt = $pdo->prepare("INSERT INTO users (username, usertag, email, password, useravatar) VALUES (:displayName, :login, :email, :password, :useravatar)");
     $stmt->bindParam(':displayName', $displayName);
     $stmt->bindParam(':login', $login);
     $stmt->bindParam(':email', $email);
     $stmt->bindParam(':password', $hashedPassword);
+    $stmt->bindParam(':useravatar', $avatar);
 
     try {
         $stmt->execute();
-        echo "POST: Registration successful";
+        // echo "POST: Registration successful";
+        header("Location: /welcome-new");
     }
     catch(PDOException $e) {
         echo "POST: Registration error: ". $e->getMessage();
---
+++ src/views/register-success.php
@@ -0,0 +1,25 @@
+<DOCTYPE !html>
+<html>
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Lora">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<script src="js/bundle.js"></script>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<script defer src="js/ds-common-buttons.js"></script>
+</head>
+<body>
+<div class="flex-container-centered ds-dashed-border" style="flex-direction:row;">
+
+    <div class="flex-container-centered">
+        <h1 class="ds-logo-container" id="homeButton">BREVITY</h1>
+    </div>
+    
+    <div class="flex-container-centered" style="align-items:stretch;">
+        <b class="ds-important-text" style="font-size:2em;">Welcome on board.</b>
+        <strong>You can now <a href="/sign-in">sign in</a>.</strong>
+    </div>
+</div>
