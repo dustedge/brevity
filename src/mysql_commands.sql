956b259e4776cab938ed04a03159c3e97a10243c
dustedge <schnapps_work@proton.me>
2024-10-26 01:49:09 +0300

    Edited template

--- src/mysql_commands.sql
+++ src/mysql_commands.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
     username VARCHAR(30) NOT NULL,
     usertag VARCHAR(30) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL, 
+    email VARCHAR(255) NOT NULL UNIQUE, 
     useravatar varchar(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
